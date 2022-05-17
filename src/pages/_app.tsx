35b0aa4b943f6dbafb2769d34f72023ef355ef19
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-05-17 20:47:02 +1000

    Install and set up `next-i18next`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ApiClientProvider, LocalisationProvider } from '@common/contexts';
+import { appWithTranslation } from 'next-i18next';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -12,4 +13,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default MyApp;
+export default appWithTranslation(MyApp);
