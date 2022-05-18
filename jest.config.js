67a2efcc5b9ed2489f498a5a1cf4173e8fa155f8
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-05-18 23:12:00 +1000

    Install & set up `jest`, `react-testing-library`

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+// Stolen from https://nextjs.org/docs/testing#jest-and-react-testing-library
+
+// jest.config.js
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './src',
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@src/(.*)$': ['<rootDir>/src/$1'],
+    '^@generated/(.*)$': ['<rootDir>/src/_generated/$1'],
+    '^@pages/(.*)$': ['<rootDir>/src/pages/$1'],
+    '^@common/(.*)$': ['<rootDir>/src/common/$1'],
+    '^@modules/(.*)$': ['<rootDir>/src/modules/$1'],
+    '^@utils/(.*)$': ['<rootDir>/src/utils/$1'],
+  },
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
