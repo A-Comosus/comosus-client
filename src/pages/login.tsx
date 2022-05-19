9512821c51aef9b59a2330c351d3d1a1c2633be0
KevinLu <lushikai277@gmail.com>
2022-05-19 15:53:28 +1000

    Change component name

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,11 @@
 import Head from 'next/head';
 import LoginForm from '@src/modules/auth/LoginForm';
-import Navlogo from '@src/modules/next-template/components/Navlogo';
+import NavLogo from '@src/modules/next-template/components/Navlogo';
 
 export default function About() {
   return (
     <div className="mainpage">
-      <Navlogo />
+      <NavLogo />
       <LoginForm />
       <div className="backgroundImage"></div>
     </div>
