8332a97efc23e396cc451db9b8ad60615317bbe8
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-05-23 09:51:48 +1000

    Update `<Text>` component to spread props

--- src/common/components/text/Text.tsx
+++ src/common/components/text/Text.tsx
@@ -1,29 +1,48 @@
 import React from 'react';
 
-import { Heading as CKHeading, Text as CKText, Code } from '@chakra-ui/react';
+import {
+  Heading as CKHeading,
+  HeadingProps,
+  Text as CKText,
+  TextProps as CKTextProps,
+  Code,
+} from '@chakra-ui/react';
 
 export type TextTypes = 'h1' | 'h2' | 'p' | 'code' | 'span';
 
 type TextProps = {
   children: React.ReactNode;
   type?: TextTypes;
-};
+} & HeadingProps &
+  CKTextProps;
 
-export default function Text({ children, type = 'p' }: TextProps) {
+export default function Text({ children, type = 'p', ...props }: TextProps) {
   const types = {
     h1: (
-      <CKHeading as="h1" fontSize="4rem">
+      <CKHeading as="h1" fontSize="4rem" {...props}>
         {children}
       </CKHeading>
     ),
-    h2: <CKHeading as="h2">{children}</CKHeading>,
-    p: <CKText as="p">{children}</CKText>,
+    h2: (
+      <CKHeading as="h2" {...props}>
+        {children}
+      </CKHeading>
+    ),
+    p: (
+      <CKText as="p" {...props}>
+        {children}
+      </CKText>
+    ),
     code: (
       <Code borderRadius="5px" p="0.75rem" fontSize="1.1rem">
         {children}
       </Code>
     ),
-    span: <CKText as="span">{children}</CKText>,
+    span: (
+      <CKText as="span" {...props}>
+        {children}
+      </CKText>
+    ),
   };
 
   return types[type];
