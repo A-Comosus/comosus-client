5a4e671f873e2c6523f6004ebd1246555879ca0f
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-05-22 16:37:30 +1000

    Refactor `<ToggleThemeButton>` using `chakra-ui`

--- src/common/components/index.ts
+++ src/common/components/index.ts
@@ -4,12 +4,10 @@ export { default as Footer } from './Footer';
 // Generic Components
 export { default as Footer } from './generics/Footer';
 export { default as SelectLanguage } from './generics/SelectLanguage';
+export { default as ToggleThemeButton } from './generics/ToggleThemeButton';
 
 export { default as Container } from './layout/Container';
 
 // Text Components
 export { default as Text } from './text/Text';
 export { default as Link } from './text/Link';
-
-// theme
-export { default as ToggleThemeButton } from './theme/ToggleThemeButton';
