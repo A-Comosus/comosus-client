26b52b7a3c0af552c554f1ee8be3d37ccd1163bd
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-05-22 17:20:50 +1000

    Localise `<NavBar>`

--- src/common/components/generics/NavBar.tsx
+++ src/common/components/generics/NavBar.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 
 import { HStack, Link, Button } from '@chakra-ui/react';
 import { SelectLanguage, ToggleThemeButton } from '@common/components';
+import { useTranslation } from 'react-i18next';
 
 export default function NavBar() {
+  const { t } = useTranslation();
+
   return (
     <HStack justify="space-between" borderBottom="1px solid #eaeaea" p="1rem">
       <HStack>
         <Link href="/">
-          <Button>Home</Button>
+          <Button>{t('nav.home')}</Button>
         </Link>
         <Link href="/space-x">
-          <Button>SpaceX</Button>
+          <Button>{t('nav.space-x')}</Button>
         </Link>
         <Link href="/disabled">
-          <Button isDisabled>Disabled</Button>
+          <Button isDisabled>{t('button.disabled')}</Button>
         </Link>
       </HStack>
 
