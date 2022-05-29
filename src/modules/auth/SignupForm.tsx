fcdc10b51a6105f4f16a02d2d7414283872d74d8
KevinLu <lushikai277@gmail.com>
2022-05-29 22:49:26 +1000

    Refactor sign up form

--- src/modules/auth/SignupForm.tsx
+++ src/modules/auth/SignupForm.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
-import Button from '../next-template/components/Button';
 import Link from 'next/link';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import { useRegisterMutation } from '@generated/graphql.queries';
-import { Logo } from '@src/common/components';
+import { FormErrorMessage, Logo, Input, Button } from '@src/common/components';
+import { FormControl, VStack } from '@chakra-ui/react';
+import { AiOutlineMail, AiOutlineUser } from 'react-icons/ai';
+import { RiLockPasswordLine } from 'react-icons/ri';
 
 interface IFormInputs {
   name: string;
@@ -20,192 +22,74 @@ const schema = yup.object().shape({
   password: yup.string().min(4).max(15).required('Please enter a password'),
 });
 
-export default function SignupForm() {
+type SignUpFormProps = {
+  onSubmit: () => void;
+  isInvalid: boolean;
+  isLoading: boolean;
+};
+
+export default function SignUpForm({
+  onSubmit,
+  isInvalid,
+  isLoading,
+}: SignUpFormProps) {
   const {
     register,
     handleSubmit,
     formState: { errors },
+    control,
   } = useForm<IFormInputs>({
     resolver: yupResolver(schema),
   });
   console.log(errors);
 
-  const onSubmit = (data: IFormInputs) => {
-    console.log(data);
+  const formValues = {
+    inputs: [
+      {
+        type: 'text',
+        name: 'username',
+        placeholder: 'Username',
+        leftElement: <AiOutlineUser />,
+      },
+      {
+        type: 'email',
+        name: 'email',
+        placeholder: 'Email',
+        leftElement: <AiOutlineMail />,
+      },
+      {
+        type: 'password',
+        name: 'password',
+        placeholder: 'Password',
+        leftElement: <RiLockPasswordLine />,
+      },
+    ],
   };
-
   return (
-    <MainContainer>
+    <VStack minW="480px" align="stretch" gap="60px">
       <Logo />
-      <Title>
-        Create an acSubTitlr free
-        <SubTitle>Free forever. No Payment needed</SubTitle>
-      </Title>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <InputContainer>
-          <Input
-            type="text"
-            placeholder="Username"
-            {...register('name', {
-              minLength: {
-                value: 4,
-                message: 'Min length is 4',
-              },
-            })}
-          />
-          <Paragraph>{errors.name?.message}</Paragraph>
-          <Input
-            type="email"
-            placeholder="Email address"
-            {...register('email')}
-          />
-          <Paragraph>{errors.email?.message}</Paragraph>
-          <Input
-            type="password"
-            placeholder="Create Password"
-            {...register('password', {
-              minLength: {
-                value: 6,
-                message: 'Min length is 6',
-              },
-            })}
-          />
-          <Paragraph>{errors.password?.message}</Paragraph>
-        </InputContainer>
-        <Conditions>
-          <Term>
-            <input type="checkbox" />
-          </Term>
-          <TermContainer>
-            <Policy>
-              By Creating an account you are agreeing our Terms and Conditions.
-            </Policy>
-            View our Privacy Notice to see how we manage your personal
-            information
-          </TermContainer>
-        </Conditions>
-        <ButtonContainer>
-          <Button content="Sign up with email" />
-        </ButtonContainer>
-        <HorizontalRule />
-      </Form>
-      <AlreadyHaveAccount>
-        <Link href="/login" passHref>
-          Already have an account?
-        </Link>
-      </AlreadyHaveAccount>
-    </MainContainer>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl isInvalid={isInvalid}>
+          <VStack align="stretch" gap="30px">
+            {formValues.inputs.map(
+              ({ type, name, placeholder, leftElement }, index) => (
+                <Input
+                  key={index}
+                  type={type}
+                  name={name}
+                  control={control}
+                  placeholder={placeholder}
+                  leftElement={leftElement}
+                />
+              ),
+            )}
+            <FormErrorMessage error={'Error'} />
+            <Button type="submit" isLoading={isLoading}>
+              Sign Up
+            </Button>
+          </VStack>
+        </FormControl>
+      </form>
+    </VStack>
   );
 }
-
-const MainContainer = styled.div`
-  max-width: 640px;
-  margin: 0px auto;
-  height: auto;
-  margin-bottom: 80px;
-`;
-
-const Title = styled.div`
-  margin: 0 0 3rem 0;
-  font-size: 2.2rem;
-  font-weight: 200;
-  color: #fff;
-`;
-
-const Paragraph = styled.p`
-  color: #f6486c;
-`;
-const SubTitle = styled.div`
-  padding-top: 1rem;
-  font-size: 1rem;
-`;
-
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  height: 20%;
-  width: 100%;
-  color: #fff;
-`;
-
-const InputContainer = styled.div`
-  display: grid;
-  gap: 12px;
-  padding-bottom: 28px;
-`;
-
-const Input = styled.input`
-  background: #f5f6f8;
-  border-radius: 0.7rem;
-  height: 3rem;
-  padding: 1rem;
-  border: none;
-  outline: none;
-  margin-bottom: 12px;
-  &:focus {
-    display: inline-block;
-    box-shadow: 0 0 0 0.14rem #000;
-    backdrop-filter: blur(12rem);
-    border-radius: 0.7rem;
-  }
-`;
-
-const ButtonContainer = styled.div`
-  margin: 3rem 0 0 0;
-  width: 100%
-  display: flex;
-  align-items:center;
-  justify-content:center;
-`;
-
-const Conditions = styled.div`
-  font-weight: lighter;
-  font-size: 0.9rem;
-  display: inline-block;
-  color: #fff;
-  display: flex;
-  width: 100%;
-  padding-bottom: 12p;
-`;
-
-const TermContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-buttom: 10px;
-`;
-
-const Privacy = styled.div`
-  text-decoration: underline;
-  display: inline-block;
-  margin-top: 1rem;
-  color: #fff;
-`;
-
-const Policy = styled.div`
-  padding-bottom: 12px;
-`;
-
-const Term = styled.div`
-  padding-right: 10px;
-`;
-
-const HorizontalRule = styled.hr`
-  border-radius: 0.8rem;
-  margin: 1.5rem 0 1rem 0;
-  background: #d8dce4;
-`;
-
-const AlreadyHaveAccount = styled.div`
-  cursor: pointer;
-  font-weight: lighter;
-  font-size: 14px;
-  color: #fff;
-  line-height: 1.6;
-  text-align: center;
-  width: 100%;
-  display: block
-  &:hover {
-    text-decoration: underline;
-  }
-`;
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,11 +1,14 @@
 import { AppContainer } from '@src/common/components';
 import SignupForm from '@src/modules/auth/SignupForm';
+import { VStack } from '@chakra-ui/react';
 
 export default function SignUp() {
   const head = { title: 'signup' };
   return (
     <AppContainer head={head}>
-      <SignupForm />
+      <VStack justify="center">
+        <SignupForm />
+      </VStack>
     </AppContainer>
   );
 }
