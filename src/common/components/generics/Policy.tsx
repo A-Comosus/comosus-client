9cba41cec26cc0c319fde78f7830e691324ae050
KevinLu <lushikai277@gmail.com>
2022-06-01 11:55:58 +1000

    Add policy component to sign up page

---
+++ src/common/components/generics/Policy.tsx
@@ -0,0 +1,23 @@
+import { FormControl, Checkbox, HStack, Text } from '@chakra-ui/react';
+import { FormErrorMessage } from '@src/common/components';
+import { Controller } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+
+export default function Policy({ name, control }) {
+  const { t } = useTranslation('auth');
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState: { invalid, error } }) => (
+        <FormControl id={name} isInvalid={invalid}>
+          <HStack alignItems="flex-start">
+            <Checkbox mt="4px" {...field} />
+            <Text>{t('signup.policy.title')}</Text>
+          </HStack>
+          {error && <FormErrorMessage error={error.message} />}
+        </FormControl>
+      )}
+    />
+  );
+}
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,13 +1,15 @@
 import { AppContainer } from '@src/common/components';
 import { SignUpForm } from '@src/modules/auth';
 import { VStack } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
 
 export default function SignUp() {
-  const head = { title: 'signup' };
+  const { t } = useTranslation('auth');
+  const head = { title: t('signup.title') };
   return (
     <AppContainer head={head}>
       <VStack justify="center">
-        <SignUpForm />
+        <SignUpForm onSubmit={() => {}} isInvalid={false} isLoading={false} />
       </VStack>
     </AppContainer>
   );
