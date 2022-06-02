b8e986fd309547a2c1b5be6fabeb1a96a86062d5
Miao Zhao <miao_zhao@yahoo.com>
2022-06-02 20:30:42 +1000

    Create `forget password` page

---
+++ src/pages/forget-password.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useApiClient } from '@common/contexts';
+import { useForgetPasswordMutation } from '@generated/graphql.queries';
+
+import { HStack, VStack } from '@chakra-ui/react';
+import { AppContainer, Logo } from '@src/common/components';
+import { ForgetPasswordForm } from '@src/modules/auth';
+import { isNil } from 'lodash';
+
+export default function ForgetPassword() {
+  const { t } = useTranslation('auth');
+  const head = { title: t('forget-password.title') };
+  const [successMessage, setSuccessMessage] = useState('');
+  const { gqlClient } = useApiClient();
+  const {
+    mutate: forgetPasswordSendEmail,
+    error,
+    isLoading: isSendingEmail,
+  } = useForgetPasswordMutation(gqlClient, {
+    onSettled: (data, error) => {
+      if (error) {
+        // @ts-ignore
+        console.error(error);
+        setSuccessMessage('');
+      }
+      if (data) {
+        setSuccessMessage(
+          'We have received your request, please check your email!',
+        );
+      }
+    },
+  });
+  const onSubmit = (value: ForgetPasswordFormTypes) => {
+    forgetPasswordSendEmail({ email: value });
+  };
+
+  return (
+    <AppContainer head={head}>
+      <VStack flex={1} padding="1rem">
+        <HStack width="100%">
+          <Logo height="5rem" />
+        </HStack>
+        <ForgetPasswordForm
+          onSubmit={onSubmit}
+          isLoading={isSendingEmail}
+          isInvalid={!isNil(error)}
+          successMessage={successMessage}
+        />
+      </VStack>
+    </AppContainer>
+  );
+}
