54349521c28e8fdf2a929ae45fa8f05137a8ecdf
Miao Zhao <miao_zhao@yahoo.com>
2022-06-02 22:51:53 +1000

    Update `Input` variants

--- src/common/components/index.ts
+++ src/common/components/index.ts
@@ -11,7 +11,6 @@ export { default as ToggleThemeButton } from './generics/ToggleThemeButton';
 // Form Components
 export { default as Button } from './forms/Button';
 export { default as Input } from './forms/Input';
-export { default as InputOutline } from './forms/InputOutline';
 export { default as FormErrorMessage } from './forms/FormErrorMessage';
 export { default as FormSuccessMessage } from './forms/FormSuccessMessage';
 
