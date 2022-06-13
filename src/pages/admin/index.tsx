3112d66f419c4655ff1e3cc7f95e4d6fead6366c
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-06-13 17:32:40 +1000

    Refactor `/admin` page with `AppContainer`

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import { useApiClient } from '@common/contexts';
 import { useGetServerInfoQuery } from '@generated/graphql.queries';
 
-import { VStack } from '@chakra-ui/react';
+import { AppContainer } from '@modules/admin/components';
+import { HStack } from '@chakra-ui/react';
 import { Text } from '@common/components';
 
 export default function Admin() {
+  const head = { title: 'A-Comosus Admin' };
+
   const { gqlClient } = useApiClient();
   const { data: serverData } = useGetServerInfoQuery(gqlClient);
 
   return (
-    <VStack minH="100vh" justify="center">
-      <Text type="h2">Admin Panel</Text>
-      <Text>{JSON.stringify(serverData, null, 2)}</Text>
-    </VStack>
+    <AppContainer head={head}>
+      <HStack>
+        <Text type="h2">Admin Panel</Text>
+        <Text>{JSON.stringify(serverData, null, 2)}</Text>
+      </HStack>
+    </AppContainer>
   );
 }
