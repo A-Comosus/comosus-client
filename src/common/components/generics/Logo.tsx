0a2af378680c4113c9faf6e51f9ff67b02f1f180
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-06-18 23:37:02 +1000

    Update `Logo` to accept `BoxProps`

--- src/common/components/generics/Logo.tsx
+++ src/common/components/generics/Logo.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-
-import { Image, HStack } from '@chakra-ui/react';
-import { Text } from '@common/components';
 import { useTranslation } from 'react-i18next';
+
+import { Image, HStack, BoxProps } from '@chakra-ui/react';
+import { Text } from '@common/components';
 
 type LogoPropsType = {
   variant?: 'block' | 'inline';
   height?: string;
-};
+} & BoxProps;
 
-export default function Logo({ variant = 'block', height }: LogoPropsType) {
+export default function Logo({
+  variant = 'block',
+  height,
+  ...props
+}: LogoPropsType) {
   const { t } = useTranslation();
   const siteTitle = t('site.title');
 
@@ -20,10 +24,11 @@ export default function Logo({ variant = 'block', height }: LogoPropsType) {
         alt={siteTitle}
         alignSelf="center"
         height={height}
+        {...props}
       />
     ),
     inline: (
-      <HStack gap={1}>
+      <HStack gap={1} {...props}>
         <Image src="/assets/logo.png" alt={siteTitle} alignSelf="center" />
         <Text>{siteTitle}</Text>
       </HStack>
