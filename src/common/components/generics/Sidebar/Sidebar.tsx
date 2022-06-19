a3cbf8229c758f4ca8926a386b9bfe54d784f4ea
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-06-19 11:00:31 +1000

    Add `Sidebar.Logout` to logout user

--- src/common/components/generics/Sidebar/Sidebar.tsx
+++ src/common/components/generics/Sidebar/Sidebar.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+
 import { IoLinkSharp } from 'react-icons/io5';
 import { BsGear } from 'react-icons/bs';
 import { SiGoogleanalytics } from 'react-icons/si';
@@ -7,19 +9,20 @@ import { BsChatSquareDots } from 'react-icons/bs';
 import { TbColorSwatch } from 'react-icons/tb';
 import { BsMegaphone } from 'react-icons/bs';
 import { MdOutlineAccountCircle } from 'react-icons/md';
-import { MdLogout } from 'react-icons/md';
-import { Avatar, VStack } from '@chakra-ui/react';
-import MenuItem from './MenuItem';
-import styles from './Sidebar.module.scss';
-import { useTranslation } from 'react-i18next';
 
+import { AppRoute } from '@src/constants/PageRoutes';
+import { Avatar, VStack } from '@chakra-ui/react';
 import { Logo } from '@common/components';
+import SidebarMenuItem from './Sidebar.MenuItem';
+import SidebarLogout from './Sidebar.Logout';
+
+import styles from './Sidebar.module.scss';
 
 function Sidebar() {
   const { t } = useTranslation();
   const navItems = [
     {
-      href: '/admin',
+      href: AppRoute.Admin,
       content: t('sidebar.link'),
       icon: <IoLinkSharp />,
     },
@@ -58,22 +61,23 @@ function Sidebar() {
       content: t('sidebar.my-account'),
       icon: <MdOutlineAccountCircle />,
     },
-    {
-      href: '/',
-      content: t('sidebar.logout'),
-      icon: <MdLogout />,
-    },
   ];
 
   return (
     <nav className={styles.sidebar}>
       <VStack flex={1} gap={10}>
-        <Avatar size="lg" src="https://picsum.photos/200" />
+        <Avatar size="xl" src="https://picsum.photos/200" />
 
         <ul className={styles.sidebar__menu}>
           {navItems.map(({ href, content, icon }, index) => (
-            <MenuItem key={index} href={href} content={content} icon={icon} />
+            <SidebarMenuItem
+              key={index}
+              href={href}
+              content={content}
+              icon={icon}
+            />
           ))}
+          <SidebarLogout />
         </ul>
       </VStack>
 
