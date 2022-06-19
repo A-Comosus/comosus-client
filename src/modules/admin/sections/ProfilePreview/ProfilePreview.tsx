aa25fd39d6007e8a60134e150538800c89c635c8
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-06-19 18:32:48 +1000

    Add `ProfilePreview` section

---
+++ src/modules/admin/sections/ProfilePreview/ProfilePreview.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { useApiClient, useUser } from '@src/common/contexts';
+import { useFindUserByUsernameQuery } from '@generated/graphql.queries';
+
+import { Avatar, HStack, IconButton, Spinner, VStack } from '@chakra-ui/react';
+import { Logo, Text } from '@src/common/components';
+import ProfilePreviewLink from './ProfilePreview.Link';
+import { BsShareFill } from 'react-icons/bs';
+import { FaShareSquare } from 'react-icons/fa';
+
+export default function ProfilePreview() {
+  const { t } = useTranslation('admin');
+  const {
+    user: { username },
+  } = useUser();
+  const { gqlClient } = useApiClient();
+  const { data: userData, isLoading } = useFindUserByUsernameQuery(
+    gqlClient,
+    { payload: { username } },
+    {
+      select: (data) => data.findUserByUsername,
+    },
+  );
+
+  return (
+    <VStack flex={1} justify="center" align="center" p="20px">
+      <VStack
+        position="relative"
+        justify="space-between"
+        overflowY="scroll"
+        __css={{
+          '&::-webkit-scrollbar': { display: 'none' },
+        }}
+        gap={20}
+        border="5px solid #3B3C46"
+        borderRadius="20px"
+        p="40px"
+        w="300px"
+        h="550px"
+        bgGradient="linear-gradient(180deg, #465E79 0%, #4B3F4F 97.92%)"
+      >
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <VStack alignSelf="stretch" gap={5}>
+            <IconButton
+              aria-label={t('link.preview.share')}
+              icon={<FaShareSquare />}
+              position="absolute"
+              top={5}
+              right={2.5}
+            />
+
+            <VStack>
+              <Avatar size="lg" src="https://picsum.photos/200" />
+              <Text color="white">{userData?.username}</Text>
+            </VStack>
+
+            <VStack alignSelf="stretch" align="stretch" gap={4}>
+              {userData &&
+                userData.links.map((link, index) => {
+                  return <ProfilePreviewLink key={index} link={link} />;
+                })}
+            </VStack>
+          </VStack>
+        )}
+
+        <Logo color="white" variant="inline" />
+      </VStack>
+
+      <HStack>
+        <BsShareFill />
+        <Text textDecoration="underline">
+          <a href="">{t('link.preview.share')}</a>
+        </Text>
+      </HStack>
+    </VStack>
+  );
+}
--- src/modules/admin/sections/index.ts
+++ src/modules/admin/sections/index.ts
@@ -1,1 +1,2 @@
 export { default as LinkEditor } from './LinkEditor/LinkEditor';
+export { default as ProfilePreview } from './ProfilePreview/ProfilePreview';
