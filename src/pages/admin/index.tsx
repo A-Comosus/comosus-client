c9563f4b9a00e84abaad50e8bb56f858505fc430
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-06-19 18:33:26 +1000

    Implement `ProfilePreview` section

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,27 +1,19 @@
 import React from 'react';
-import { useApiClient } from '@common/contexts';
 import { useTranslation } from 'react-i18next';
-import { useGetServerInfoQuery } from '@generated/graphql.queries';
 
 import { AppContainer } from '@modules/admin/components';
-import { HStack, VStack } from '@chakra-ui/react';
-import { LinkEditor } from '@modules/admin/sections';
-import { Text } from '@common/components';
+import { HStack } from '@chakra-ui/react';
+import { LinkEditor, ProfilePreview } from '@modules/admin/sections';
 
 export default function Admin() {
   const { t } = useTranslation('admin');
   const head = { title: t('admin.title') };
-  const { gqlClient } = useApiClient();
-  const { data: serverData } = useGetServerInfoQuery(gqlClient);
 
   return (
     <AppContainer head={head}>
       <HStack flex={1} align="stretch">
         <LinkEditor />
-        <VStack flex={1}>
-          <Text type="h2">{t('admin.title')}</Text>
-          <Text>{JSON.stringify(serverData, null, 2)}</Text>
-        </VStack>
+        <ProfilePreview />
       </HStack>
     </AppContainer>
   );
