d13cea9de91f46777aab6eeb7773fd65d715aab5
Miao Zhao <miao_zhao@yahoo.com>
2022-06-26 20:17:52 +1000

    Update reset-password page by removing unnecessary components

--- src/pages/reset-password/[resettoken].tsx
+++ src/pages/reset-password/[resettoken].tsx
@@ -1,29 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useRouter } from 'next/router';
 import { useApiClient } from '@common/contexts';
 
 import { HStack, VStack } from '@chakra-ui/react';
-import { AppContainer, Logo } from '@src/common/components';
+import { PageContainer, Logo } from '@src/common/components';
 import { ResetPasswordForm } from '@src/modules/auth';
-import { AuthRoute } from '@src/constants/PageRoutes';
 import { useResetPasswordMutation } from '@generated/graphql.queries';
+import { AuthRoute } from '@src/constants/PageRoutes';
 import { isNil } from 'lodash';
 
 export default function ForgetPassword() {
   const { t } = useTranslation('auth');
   const { gqlClient } = useApiClient();
+  const router = useRouter();
   const head = { title: t('reset-password.title') };
 
-  const [resetToken, setResetToken] = useState('');
-  const [successMessage, setSuccessMessage] = useState('');
-  const [successAction, setSuccessAction] = useState('');
-  const [successActionLink, setSuccessActionLink] = useState('');
-
-  const token = useRouter().query.resettoken as string;
-  useEffect(() => {
-    !isNil(token) && setResetToken(token);
-  }, [token]);
+  const {
+    query: { resetToken },
+  } = router;
+  const token = resetToken as string;
 
   const {
     mutate: resetPassword,
@@ -33,12 +29,9 @@ export default function ForgetPassword() {
     onSettled: (data, error) => {
       if (error) {
         console.error(error);
-        setSuccessMessage('');
       }
       if (data) {
-        setSuccessMessage(t('reset-password.success'));
-        setSuccessAction(t('reset-password.success-action'));
-        setSuccessActionLink(AuthRoute.Login);
+        router.push(AuthRoute.Login);
       }
     },
   });
@@ -47,13 +40,13 @@ export default function ForgetPassword() {
     resetPassword({
       detail: {
         password: value.password,
-        resetToken,
+        resetToken: token,
       },
     });
   };
 
   return (
-    <AppContainer head={head}>
+    <PageContainer head={head}>
       <VStack flex={1} padding="1rem">
         <HStack width="100%">
           <Logo height="5rem" />
@@ -62,11 +55,8 @@ export default function ForgetPassword() {
           onSubmit={onSubmit}
           isLoading={isResetingEmail}
           isInvalid={!isNil(error)}
-          successMessage={successMessage}
-          successAction={successAction}
-          successActionLink={successActionLink}
         />
       </VStack>
-    </AppContainer>
+    </PageContainer>
   );
 }
