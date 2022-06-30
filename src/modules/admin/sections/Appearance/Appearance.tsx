b2bc1dc7daa77dd12602f1147927115248f7e598
Paul XU <pyc.xuu@gmail.com>
2022-06-30 19:36:30 +1000

    Fix prettier problem

--- src/modules/admin/sections/Appearance/Appearance.tsx
+++ src/modules/admin/sections/Appearance/Appearance.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { VStack } from '@chakra-ui/react';
-import AppearanceProfile from './Appearance.Profile'; 
+import AppearanceProfile from './Appearance.Profile';
 
 export default function Appearance() {
-
   return (
     <VStack flex={2} borderRight="1px solid #E7E8EE" pt="80px" spacing={25}>
-        <AppearanceProfile />
+      <AppearanceProfile />
     </VStack>
   );
 }
--- src/modules/admin/sections/index.ts
+++ src/modules/admin/sections/index.ts
@@ -1,3 +1,3 @@
 export { default as LinkEditor } from './LinkEditor/LinkEditor';
 export { default as ProfilePreview } from './ProfilePreview/ProfilePreview';
-export { default as Appearance} from './Appearance/Appearance';
+export { default as Appearance } from './Appearance/Appearance';
