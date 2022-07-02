008d39c8c3a73d13903a72f3f90830db98ae3e1c
Rita Mou <ritamxy@gmail.com>
2022-07-02 21:44:32 +1000

    Create error-page

---
+++ src/pages/error-page.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { HStack, VStack } from '@chakra-ui/react';
+import { PageContainer, Logo } from '@src/common/components';
+import { ErrorPage } from '@src/modules/auth';
+
+export default function NotifyForgetPasswordSuccess() {
+  const { t } = useTranslation('auth');
+  const head = { title: t('error-page.title') };
+
+  return (
+    <PageContainer head={head}>
+      <VStack flex={1} padding="1rem">
+        <HStack width="100%">
+          <Logo height="5rem" />
+        </HStack>
+        <ErrorPage />
+      </VStack>
+    </PageContainer>
+  );
+}
