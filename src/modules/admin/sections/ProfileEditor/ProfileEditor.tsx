71a0fc13b10d1332febd56e8e1bd87e6c43914cd
Paul XU <pyc.xuu@gmail.com>
2022-07-05 20:03:57 +1000

    Add Profile title

--- src/modules/admin/sections/ProfileEditor/ProfileEditor.tsx
+++ src/modules/admin/sections/ProfileEditor/ProfileEditor.tsx
@@ -5,6 +5,7 @@ import { useTranslation } from 'react-i18next';
 import { Text, Button, Textarea } from '@common/components';
 import {
   Avatar,
+  Stack,
   HStack,
   VStack,
   Input,
@@ -33,50 +34,55 @@ export default function ProfileEditor() {
     );
 
   return (
-    <VStack
-      px="2.2rem"
-      py="1.6rem"
-      backgroundColor="#FFFFFF"
-      borderRadius="15px"
-      minWidth="670px"
-      spacing="1.6rem"
-    >
-      <HStack width="100%">
-        <Avatar size="xl" mx="2rem" src="https://picsum.photos/200" />
-        <HStack flex={1} spacing="1rem">
-          <Button flex={1} borderRadius="10px">
-            {t('appearance.profile.pick-an-image')}
-          </Button>
-          <Button flex={1} borderRadius="10px">
-            {t('appearance.profile.remove')}
-          </Button>
+    <Stack>
+      <Text fontSize="20px" fontWeight="600" alignSelf="flex-start">
+        {t('appearance.profile.title')}
+      </Text>
+      <VStack
+        px="2.2rem"
+        py="1.6rem"
+        backgroundColor="#FFFFFF"
+        borderRadius="15px"
+        minWidth="670px"
+        spacing="1.6rem"
+      >
+        <HStack width="100%">
+          <Avatar size="xl" mx="2rem" src="https://picsum.photos/200" />
+          <HStack flex={1} spacing="1rem">
+            <Button flex={1} borderRadius="10px">
+              {t('appearance.profile.pick-an-image')}
+            </Button>
+            <Button flex={1} borderRadius="10px">
+              {t('appearance.profile.remove')}
+            </Button>
+          </HStack>
         </HStack>
-      </HStack>
 
-      <VStack width="100%" align="start">
-        <Text fontSize="12px" fontWeight="400">
-          {t('appearance.profile.profile-title')}
-        </Text>
-        <Input
-          variant="unstyled"
-          borderBottom="1px solid #ADB2C6"
-          borderRadius="0"
-          defaultValue={profileTitle}
-          placeholder={t('appearance.profile.profile-title')}
-          onBlur={(e) => setProfileTitile(e.target.value)}
-        />
-      </VStack>
+        <VStack width="100%" align="start">
+          <Text fontSize="12px" fontWeight="400">
+            {t('appearance.profile.profile-title')}
+          </Text>
+          <Input
+            variant="unstyled"
+            borderBottom="1px solid #ADB2C6"
+            borderRadius="0"
+            defaultValue={profileTitle}
+            placeholder={t('appearance.profile.profile-title')}
+            onBlur={(e) => setProfileTitile(e.target.value)}
+          />
+        </VStack>
 
-      <VStack width="100%" align="start">
-        <Text fontSize="12px" fontWeight="400">
-          {t('appearance.profile.bio')}
-        </Text>
-        <Textarea
-          defaultValue={bio}
-          placeholder={t('appearance.profile.bio-placeholder')}
-          onBlur={(e) => setBio(e.target.value)}
-        />
+        <VStack width="100%" align="start">
+          <Text fontSize="12px" fontWeight="400">
+            {t('appearance.profile.bio')}
+          </Text>
+          <Textarea
+            defaultValue={bio}
+            placeholder={t('appearance.profile.bio-placeholder')}
+            onBlur={(e) => setBio(e.target.value)}
+          />
+        </VStack>
       </VStack>
-    </VStack>
+    </Stack>
   );
 }
