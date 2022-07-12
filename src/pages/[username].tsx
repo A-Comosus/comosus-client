adb93a869f1ad1a98041ce2660eaa586fe8a9011
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-07-12 23:38:05 +1000

    Add page to display user's public profile

---
+++ src/pages/[username].tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { request } from 'graphql-request';
+
+import { Avatar, VStack } from '@chakra-ui/react';
+import { Text, ProfileItem } from '@src/common/components';
+
+type ServerSideContextType = { query: { username: string } };
+export async function getServerSideProps(context: ServerSideContextType) {
+  const { username } = context.query;
+
+  const gqlEndpoint = process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT ?? '';
+  const { findUserByUsername } = await request(
+    gqlEndpoint,
+    `
+    query FindByUsername($payload: FindUserByUsernameInput!) {
+      findUserByUsername(username: $payload) {
+        id
+        displayName
+        username
+        links {
+          id
+          title
+          url
+          logoUrl
+        }
+      }
+    }
+    `,
+    { payload: { username } },
+  );
+
+  return {
+    props: { userData: findUserByUsername }, // will be passed to the page component as props
+  };
+}
+
+type PublicProfileProps = {
+  userData: {
+    id: string;
+    displayName: string;
+    username: string;
+    links: {
+      id: string;
+      title: string;
+      url: string;
+    }[];
+  };
+};
+export default function PublicProfile({ userData }: PublicProfileProps) {
+  return (
+    <VStack
+      p="10rem"
+      minH="100vh"
+      bgGradient="linear-gradient(180deg, #465E79 0%, #4B3F4F 97.92%)"
+    >
+      <VStack alignSelf="stretch" gap={5}>
+        <VStack>
+          <Avatar size="lg" src="https://picsum.photos/200" />
+          <Text color="white">
+            {userData?.displayName ?? userData?.username}
+          </Text>
+        </VStack>
+
+        <VStack alignSelf="stretch" align="stretch" gap={4}>
+          {userData &&
+            userData.links.map((link, index) => {
+              return <ProfileItem key={index} link={link} />;
+            })}
+        </VStack>
+      </VStack>
+    </VStack>
+  );
+}
