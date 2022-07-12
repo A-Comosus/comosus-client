8d7785cb4f231d149a7a205a47e19e4fcfdd48e1
Paul XU <pyc.xuu@gmail.com>
2022-07-12 20:55:14 +1000

    Refractor ProfileEditor

--- src/modules/admin/sections/ProfileEditor/ProfileEditor.tsx
+++ src/modules/admin/sections/ProfileEditor/ProfileEditor.tsx
@@ -1,31 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { useUser } from '@src/common/contexts';
+import React, { useMemo } from 'react';
+import { useApiClient, useUser } from '@src/common/contexts';
 import { useTranslation } from 'react-i18next';
+import { useFindUserByUsernameQuery } from '@generated/graphql.queries';
 
-import ProfileEditorAvatar from './ProfileEditor.Avatar';
-import { Text, Textarea } from '@common/components';
-import {
-  VStack,
-  Input,
-  Box,
-  SkeletonCircle,
-  SkeletonText,
-  useToast,
-} from '@chakra-ui/react';
+import { Text } from '@common/components';
+import { VStack, Box, SkeletonCircle, SkeletonText } from '@chakra-ui/react';
+import EditableProfile from './EditableProfile/EditableProfile';
+import _ from 'lodash';
 
 export default function ProfileEditor() {
   const { t } = useTranslation('admin');
-  const toast = useToast();
   const {
     user: { username },
   } = useUser();
-  const [profileTitle, setProfileTitle] = useState('');
-  const [bio, setBio] = useState('');
-  useEffect(() => {
-    setProfileTitle(username);
-  }, [username]);
 
-  if (!username) {
+  const enabled = useMemo(() => {
+    return !_.isNil(username);
+  }, [username]);
+  const { gqlClient } = useApiClient();
+  const { data: userData, isLoading } = useFindUserByUsernameQuery(
+    gqlClient,
+    { payload: { username } },
+    {
+      enabled,
+      select: (data) => data.findUserByUsername,
+    },
+  );
+  const profile = {
+    username: username,
+    displayName: userData?.displayName,
+    bio: userData?.bio,
+  };
+
+  if (!username || isLoading) {
     return (
       <Box minWidth="670px" padding="6" boxShadow="lg" bg="white">
         <SkeletonCircle size="10" />
@@ -39,52 +46,7 @@ export default function ProfileEditor() {
       <Text type="h3" alignSelf="flex-start">
         {t('appearance.profile.title')}
       </Text>
-      <VStack
-        align="stretch"
-        gap="1.6rem"
-        borderRadius="15px"
-        px="2.2rem"
-        py="1.6rem"
-        minWidth="670px"
-        backgroundColor="#FFFFFF"
-      >
-        <ProfileEditorAvatar />
-        <VStack>
-          <Text type="p" alignSelf="flex-start">
-            {t('appearance.profile.profile-title')}
-          </Text>
-          <Input
-            variant="unstyled"
-            borderBottom="1px solid #ADB2C6"
-            borderRadius="0"
-            placeholder={t('appearance.profile.profile-title')}
-            value={profileTitle}
-            onChange={(e) => setProfileTitle(e.target.value)}
-            onBlur={() =>
-              toast({
-                status: 'success',
-                title: `Submitting Profile Title: ${profileTitle}`,
-                variant: 'subtle',
-              })
-            }
-          />
-        </VStack>
-
-        <Textarea
-          label={t('appearance.profile.bio')}
-          placeholder={t('appearance.profile.bio-placeholder')}
-          value={bio}
-          maxLength={100}
-          onChange={(e) => setBio(e.target.value)}
-          onBlur={() =>
-            toast({
-              status: 'success',
-              title: `Submitting Bio Description: ${bio}`,
-              variant: 'subtle',
-            })
-          }
-        />
-      </VStack>
+      <EditableProfile profile={profile} />
     </VStack>
   );
 }
