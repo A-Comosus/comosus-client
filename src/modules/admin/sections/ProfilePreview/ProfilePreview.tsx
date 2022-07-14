c8f2fbc9836076cd2d7565171f7f71345f0cca89
Paul XU <pyc.xuu@gmail.com>
2022-07-14 17:59:02 +1000

    Update logic to display username when displayName is empty

--- src/modules/admin/sections/ProfilePreview/ProfilePreview.tsx
+++ src/modules/admin/sections/ProfilePreview/ProfilePreview.tsx
@@ -61,7 +61,7 @@ export default function ProfilePreview() {
             <VStack>
               <Avatar size="lg" src="https://picsum.photos/200" />
               <Text color="white">
-                {userData?.displayName ?? userData?.username}
+                {userData?.displayName || userData?.username}
               </Text>
               {userData?.bio && <Text color="white">{userData?.bio}</Text>}
             </VStack>
