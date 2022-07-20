76e951324c0f87598f9a85468359be31a5b715f2
Paul XU <pyc.xuu@gmail.com>
2022-07-20 21:10:07 +1000

    Update localization

--- src/modules/admin/sections/LinkEditor/EditableLink/EditableLink.Delete.tsx
+++ src/modules/admin/sections/LinkEditor/EditableLink/EditableLink.Delete.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { Button, Text } from '@src/common/components';
 import { VStack, HStack } from '@chakra-ui/react';
@@ -13,12 +14,13 @@ export default function EditableLinkDelete({
   handleDelete,
   setShowDelete,
 }: EditableLinkDeleteProps) {
+  const { t } = useTranslation('admin');
   return (
     <VStack borderRadius={4} p={[5, 10, 4, 4]} bg="white" align="stretch">
       <HStack justify="center">
         <BiChevronUp />
       </HStack>
-      <Text alignSelf="center">Delete this forever?</Text>
+      <Text alignSelf="center">{t('link.editor.toggle.prompt')}</Text>
       <HStack gap={5} px={10}>
         <Button
           flex={1}
@@ -28,7 +30,7 @@ export default function EditableLinkDelete({
           background="#DADEE0"
           onClick={() => setShowDelete(false)}
         >
-          <Text color="black">Cancel</Text>
+          <Text color="black">{t('link.editor.toggle.cancel')}</Text>
         </Button>
         <Button
           flex={1}
@@ -38,7 +40,7 @@ export default function EditableLinkDelete({
           background="#3B3C46"
           onClick={handleDelete}
         >
-          <Text>Delete</Text>
+          <Text>{t('link.editor.toggle.delete')}</Text>
         </Button>
       </HStack>
     </VStack>
