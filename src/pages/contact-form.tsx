8d532dbb4010fc958402ff3cfaea71f95d99f640
Rita Mou <58613546+ritamxy@users.noreply.github.com>
2022-07-28 23:14:30 +1000

    [AC-99: Feature] Create a Contact Form (#83)
    * create contact form
    
    * Create Chinese translation for contact form
    
    * Create English translation for contact-form
    
    * adjust layout for 404 page
    
    * adjust layout for forgetPasswordSuccess Page
    
    * create Chinese translation for contact-form
    
    * create English translation for contact-form
    
    * create contact-form page
    
    * create ContactForm
    
    * refactor contact form
    
    * delete unused files
    
    * Add Chinese translation for success message
    
    * Add English translation for success message
    
    * adjust color for success message
    
    * fix typo

---
+++ src/pages/contact-form.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { isNil } from 'lodash';
+import { useTranslation } from 'react-i18next';
+import { PageContainer } from '@src/common/components';
+import { ContactForm } from '@src/modules/auth';
+import { useApiClient } from '@common/contexts';
+import { useContactFormMutation } from '@generated/graphql.queries';
+
+export default function SendContactForm() {
+  const { t } = useTranslation('auth');
+  const head = { title: t('contact-form.title') };
+  const [emailSent, setEmailSent] = useState(false);
+
+  const { gqlClient } = useApiClient();
+  const {
+    mutate: contactForm,
+    error,
+    isLoading: isSendingEmail,
+  } = useContactFormMutation(gqlClient, {
+    onSettled: (data, error) => {
+      if (error) {
+        // @ts-ignore
+        console.error(error);
+      } else if (data) {
+        setEmailSent(true);
+      }
+    },
+  });
+  const onSubmit = (values: ContactFormValuesTypes) => {
+    contactForm({
+      detail: {
+        ...values,
+      },
+    });
+  };
+  return (
+    <PageContainer head={head}>
+      <ContactForm
+        onSubmit={onSubmit}
+        isInvalid={!isNil(error)}
+        isLoading={isSendingEmail}
+        emailSent={emailSent}
+      />
+    </PageContainer>
+  );
+}
