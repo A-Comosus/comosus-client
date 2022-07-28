aef02d78ec82305211137d6fb8823d58b0f664cc
Rita Mou <ritamxy@gmail.com>
2022-07-28 23:10:27 +1000

    fix typo

--- src/modules/auth/ContactForm/ContactForm.tsx
+++ src/modules/auth/ContactForm/ContactForm.tsx
@@ -1,4 +1,4 @@
-import { React } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
--- src/pages/contact-form.tsx
+++ src/pages/contact-form.tsx
@@ -1,4 +1,4 @@
-import { React, useState } from 'react';
+import React, { useState } from 'react';
 import { isNil } from 'lodash';
 import { useTranslation } from 'react-i18next';
 import { PageContainer } from '@src/common/components';
