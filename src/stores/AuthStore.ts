f25a786677410f155195db071c7952ba369b7244
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-08-01 22:21:18 +1000

    Fix `AuthStore` were duplicating

--- src/stores/AuthStore.ts
+++ src/stores/AuthStore.ts
@@ -1,6 +1,8 @@
-import { GlobalRoute } from '@src/constants';
-import { createStore, StoreActions } from '@src/stores';
+import create from 'zustand';
+import { devtools, persist, combine } from 'zustand/middleware';
+import { StoreActions } from '@src/stores';
 import { useRouter } from 'next/router';
+import { GlobalRoute } from '@src/constants';
 
 export type AuthStoreState = {
   id: string | null;
@@ -9,38 +11,38 @@ export type AuthStoreState = {
 
 export type AuthStoreAction = {
   initStore: (value: AuthStoreState) => void;
-  setId: (id: string) => void;
-  setAccessToken: (accessToken: string) => void;
   logout: () => void;
 };
 
 export enum AuthStore {
   Name = 'AuthStore',
   InitStore = 'InitStore',
-  SetId = 'SetId',
-  SetAccessToken = 'SetAccessToken',
   Logout = 'Logout',
 }
 
+const name = AuthStore.Name;
+const initialState: AuthStoreState = {
+  id: null,
+  accessToken: null,
+};
+const actions: StoreActions<AuthStoreState, AuthStoreAction> = (set) => ({
+  initStore: (value) => set(() => value, false, AuthStore.InitStore),
+  logout: () => {
+    set(() => initialState, false, AuthStore.Logout);
+  },
+});
+const useStore = create(
+  devtools(persist(combine(initialState, actions), { name }), { name }),
+);
+
 export function useAuth() {
-  const { push } = useRouter();
-  const name = AuthStore.Name;
-  const initialState: AuthStoreState = {
-    id: null,
-    accessToken: null,
+  const state = useStore();
+  const router = useRouter();
+
+  const logout = () => {
+    state.logout();
+    router.push(GlobalRoute.Root);
   };
-  const actions: StoreActions<AuthStoreState, AuthStoreAction> = (set) => ({
-    initStore: (value) => set(() => value, false, AuthStore.InitStore),
-    setId: (id) => set(() => ({ id }), false, AuthStore.SetId),
-    setAccessToken: (accessToken) =>
-      set(() => ({ accessToken }), false, AuthStore.SetAccessToken),
-    logout: () => {
-      set(() => initialState, false, AuthStore.Logout);
-      push(GlobalRoute.Root);
-    },
-  });
 
-  const useStore = createStore(initialState, actions, name);
-
-  return useStore();
+  return { ...state, logout };
 }
