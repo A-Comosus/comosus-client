809ec84ba2532a9ed15a48ac050623eb749f57d6
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-08-13 13:54:49 +1000

    Add type definition for alchemy nft endpoint

--- src/modules/nft/nft.d.ts
+++ src/modules/nft/nft.d.ts
@@ -2,3 +2,47 @@ declare interface Window {
   ethereum: any;
   contract: any;
 }
+
+type OwnedNFTs = {
+  ownedNfts: NFT[];
+  totalCount: number;
+  blockHash: string;
+};
+
+type NFT = {
+  contract: {
+    address: string;
+  };
+  id: {
+    tokenId: string;
+    tokenMetadata: {
+      tokenType: 'ERC721';
+    };
+  };
+  balance: string;
+  title: string;
+  description: string;
+  tokenUri: {
+    raw: string;
+    gateway: string;
+  };
+  media: {
+    raw: string;
+    gateway: string;
+  }[];
+  metadata: {
+    date: number;
+    image: string;
+    dna: string;
+    name: string;
+    description: string;
+    edition: number;
+    attributes: {
+      value: string;
+      trait_type: string;
+    }[];
+    compiler: string;
+  };
+  timeLastUpdated: string;
+  error?: string;
+};
