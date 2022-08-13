4ab21bad513f4086dc29532f3321c242f2735bb3
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-08-13 13:55:59 +1000

    Assemble nft sections with placeholder data

--- src/pages/admin/nft.tsx
+++ src/pages/admin/nft.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { AppContainer } from '@modules/admin/components';
-import { Wallet } from '@modules/nft';
+import { Wallet, OwnedNFTs } from '@modules/nft';
 import { VStack } from '@chakra-ui/react';
+
+import nftData from '@modules/nft/fakeNFTs.json';
 
 export default function NFT() {
   const { t } = useTranslation('admin');
@@ -11,8 +13,16 @@ export default function NFT() {
 
   return (
     <AppContainer head={head}>
-      <VStack flex={2} gap="1.6rem" borderRight="1px solid #E7E8EE" pt="5rem">
+      <VStack
+        flex={2}
+        align="stretch"
+        gap="1.6rem"
+        borderRight="1px solid #E7E8EE"
+        p="8rem 5.5rem"
+        overflowY="scroll"
+      >
         <Wallet />
+        <OwnedNFTs nfts={nftData.ownedNfts} />
       </VStack>
     </AppContainer>
   );
