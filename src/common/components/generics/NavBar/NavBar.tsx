92b67865873ff9ed7152cc5ad1c97cfbcd8e332c
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-08-17 23:52:01 +1000

    Update `NavBar` design

--- src/common/components/generics/NavBar/NavBar.tsx
+++ src/common/components/generics/NavBar/NavBar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
-import { HStack } from '@chakra-ui/react';
+import { Center, HStack } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 import { Logo, Link, Button, SelectLanguage } from '@common/components';
 import { GlobalRoute, AuthRoute, AppRoute } from '@src/constants/PageRoutes';
@@ -28,32 +28,34 @@ export function NavBar({ disableNavOptions }: NavBarProps) {
   ];
 
   return (
-    <HStack
-      justify="space-between"
-      alignItems="center"
-      spacing="0"
-      padding="1.2rem"
-      w="100%"
-    >
-      <Logo />
+    <Center zIndex={999} position="sticky" top={0} bg="#1B181E">
+      <HStack
+        justify="space-between"
+        spacing="0"
+        padding="1.2rem"
+        w="clamp(62.5%, 120rem, 90%)"
+      >
+        <Logo />
 
-      <HStack spacing="5rem">
-        {!disableNavOptions &&
-          navItems.map(({ href, content }) => (
-            <Link key={href} href={href}>
-              {content}
-            </Link>
-          ))}
-        <Link href={AuthRoute.Login}>{t('nav.login')}</Link>
-        <Button
-          onClick={() => push(AuthRoute.SignUp)}
-          variant="gradient"
-          size="lg"
-        >
-          {t('nav.sign-up')}
-        </Button>
-        <SelectLanguage />
+        <HStack spacing="5rem">
+          {!disableNavOptions &&
+            navItems.map(({ href, content }) => (
+              <Link key={href} href={href}>
+                {content}
+              </Link>
+            ))}
+          <Link href={AuthRoute.Login}>{t('nav.login')}</Link>
+
+          <Button
+            onClick={() => push(AuthRoute.SignUp)}
+            variant="gradient"
+            size="lg"
+          >
+            {t('nav.sign-up')}
+          </Button>
+          <SelectLanguage />
+        </HStack>
       </HStack>
-    </HStack>
+    </Center>
   );
 }
