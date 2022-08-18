2c6a18a5d5dca86f7dde02a5dece3b6b7a0bd34a
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-08-18 19:40:53 +1000

    Decouple `NavDrawer` from `NavBar`

--- src/common/components/generics/NavBar/NavBar.tsx
+++ src/common/components/generics/NavBar/NavBar.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
-import {
-  Center,
-  Drawer,
-  DrawerBody,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerHeader,
-  DrawerOverlay,
-  HStack,
-  IconButton,
-  VStack,
-} from '@chakra-ui/react';
+import { Center, HStack, IconButton } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 import { useToggle } from '@src/utils/hooks';
 import { GlobalRoute, AuthRoute, AppRoute } from '@src/constants/PageRoutes';
 
 import { Logo, Link, Button, Icon, SelectLanguage } from '@common/components';
+import { NavDrawer } from './NavBar.Drawer';
 
 export type NavBarProps = {
   disableNavOptions?: boolean;
@@ -27,18 +17,26 @@ export function NavBar({ disableNavOptions }: NavBarProps) {
   const { push } = useRouter();
   const [isOpen, toggleOpen] = useToggle();
 
-  const navItems = [
+  const navItems: NavItem[] = [
     {
-      href: GlobalRoute.Root,
-      content: t('nav.community'),
-    },
-    {
+      type: 'link',
       href: GlobalRoute.Project,
       content: t('nav.project'),
     },
     {
+      type: 'link',
       href: AppRoute.Contact,
       content: t('nav.contact'),
+    },
+    {
+      type: 'link',
+      href: AuthRoute.Login,
+      content: t('nav.login'),
+    },
+    {
+      type: 'button',
+      href: AuthRoute.SignUp,
+      content: t('nav.sign-up'),
     },
   ];
 
@@ -65,20 +63,21 @@ export function NavBar({ disableNavOptions }: NavBarProps) {
             listStyleType="none"
           >
             {!disableNavOptions &&
-              navItems.map(({ href, content }) => (
-                <Link key={href} href={href}>
-                  {content}
-                </Link>
-              ))}
-            <Link href={AuthRoute.Login}>{t('nav.login')}</Link>
-
-            <Button
-              onClick={() => push(AuthRoute.SignUp)}
-              variant="gradient"
-              size="lg"
-            >
-              {t('nav.sign-up')}
-            </Button>
+              navItems.map(({ type, href, content }) =>
+                type === 'link' ? (
+                  <Link key={href} href={href}>
+                    {content}
+                  </Link>
+                ) : (
+                  <Button
+                    onClick={() => push(href)}
+                    variant="gradient"
+                    size="lg"
+                  >
+                    {content}
+                  </Button>
+                ),
+              )}
             <SelectLanguage />
           </HStack>
 
@@ -96,40 +95,12 @@ export function NavBar({ disableNavOptions }: NavBarProps) {
           />
         </HStack>
       </Center>
-
-      <Drawer isOpen={isOpen} placement="right" onClose={toggleOpen}>
-        <DrawerOverlay />
-        <DrawerContent bg="#272429">
-          <DrawerCloseButton />
-
-          <DrawerBody>
-            <VStack
-              as="ul"
-              alignItems="flex-start"
-              spacing="2rem"
-              py="2rem"
-              listStyleType="none"
-            >
-              {!disableNavOptions &&
-                navItems.map(({ href, content }) => (
-                  <Link key={href} href={href}>
-                    {content}
-                  </Link>
-                ))}
-
-              <Button
-                onClick={() => push(AuthRoute.SignUp)}
-                variant="gradient"
-                size="lg"
-              >
-                {t('nav.sign-up')}
-              </Button>
-
-              <SelectLanguage />
-            </VStack>
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
+      <NavDrawer
+        isOpen={isOpen}
+        toggleOpen={toggleOpen}
+        navItems={navItems}
+        disableNavOptions={disableNavOptions}
+      />
     </>
   );
 }
